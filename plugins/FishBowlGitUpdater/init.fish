83ec4ad95b4c81301a96a697550b3e2d77833bc3
Martijn de Boer <github@sexybiggetje.nl>
2014-01-01 03:37:25 +0100

    Update check code

--- plugins/FishBowlGitUpdater/init.fish
+++ plugins/FishBowlGitUpdater/init.fish
@@ -3,6 +3,15 @@ function checkForFishBowlUpdate --description "Check for updates when using a gi
     cd $FBPATH
 
     if is_git
-        printf 'Is git!'
+        set -l localVersion (git rev-list --max-count=1 master)
+        set -l remoteVersion (git rev-list --max-count=1 origin/master)
+        printf 'Local version:  %s\n' $localVersion
+        printf 'Remote version: %s\n' $remoteVersion
+
+        if test $localVersion != $remoteVersion
+            printf '\nUpdates are available. Please run git pull ~/.config/fish-bowl\n'
+        end
     end
+
+    cd $currentPath
 end
